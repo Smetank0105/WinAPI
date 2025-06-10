5d7c7f030f2408ec4df70755cf9595339b075256
Smetank0105 <206859408+Smetank0105@users.noreply.github.com>
2025-06-10 17:20:22 +0700

    EditControl Login DONE

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -19,7 +19,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdline, IN
 	);
 #endif // MESSAGE_BOX
 
-	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, DlgProc, 0);
+	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
 
 	return 0;
 }
@@ -29,19 +29,31 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:	//выполняется один раз при запуске окна
-		break;
+	{
+		SendDlgItemMessage(hwnd, IDC_EDIT_LOGIN, WM_SETTEXT, 0, (LPARAM)"Введите имя пользователя");
+	}
+	break;
 	case WM_COMMAND:	//обрабатывает нажатие кнопок, перемещения мыши и тд
 		switch (LOWORD(wParam)) {
+		case IDC_EDIT_LOGIN:
+			if (HIWORD(wParam) == EN_SETFOCUS)
+				SendDlgItemMessage(hwnd, IDC_EDIT_LOGIN, WM_SETTEXT, 0, 0);
+			if (!SendDlgItemMessage(hwnd,IDC_EDIT_LOGIN,EM_LINELENGTH,0,0))
+			{
+				if (HIWORD(wParam) == EN_KILLFOCUS)
+					SendDlgItemMessage(hwnd, IDC_EDIT_LOGIN, WM_SETTEXT, 0, (LPARAM)"Введите имя пользователя");
+			}
+			break;
 		case IDC_BUTTON_COPY:
 		{
-			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
-			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
 			CONST INT SIZE = 256;
 			CHAR sz_buffer[SIZE] = {};
+			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			HWND hEditPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
 			SendMessage(hEditLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
 			SendMessage(hEditPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 		}
-			break;
+		break;
 		case IDOK:
 			MessageBox(hwnd, "Была нажата кнопка ОК", "Info", MB_OK | MB_ICONINFORMATION);
 			break;
